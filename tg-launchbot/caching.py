114185daec0803af123d090cb3cdc331e3334371
499602D2 <46869890+499602D2@users.noreply.github.com>
2020-10-18 15:23:38 +0300

    Create caching.py
    A simple skeleton of a caching model for speeding up the next and schedule commands. May or may not be used in the future.

---
+++ tg-launchbot/caching.py
@@ -0,0 +1,16 @@
+import pymemcache
+
+'''
+Basic caching logic
+
+Cache...
+	- /next calls: everything can be cached, but T- must be computed again for each instance: fast, easy
+	- /schedule call, for both types
+'''
+
+def set_memcache():
+	return
+
+
+def update_memcache():
+	return
