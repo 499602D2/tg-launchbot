1d86c2a091112b14547bd9aa7517a34e61d89991
499602D2 <46869890+499602D2@users.noreply.github.com>
2020-10-22 17:14:08 +0300

    Add two helper functions
    - added shorten_monospaced_text
    
    - added map_country_code_to_flag

--- tg-launchbot/utils.py
+++ tg-launchbot/utils.py
@@ -1,7 +1,7 @@
 import datetime
 
 def reconstruct_link_for_markdown(link: str) -> str:
-	'''Summary
+	''' Summary
 	Telegram's MarkdownV2 requires some special handling, so
 	parse the link here into a compatible format.
 	'''
@@ -16,7 +16,7 @@ def reconstruct_link_for_markdown(link: str) -> str:
 
 
 def reconstruct_message_for_markdown(message: str) -> str:
-	'''Summary
+	''' Summary
 	Performs effectively the same functions as reconstruct_link, but
 	is intended to be used for message body text.
 	'''
@@ -31,8 +31,26 @@ def reconstruct_message_for_markdown(message: str) -> str:
 	return message_reconstruct
 
 
+def shorten_monospaced_text(text: str) -> str:
+	''' Summary
+	Telegram has extremely wide spaces for the monospaced font. This function
+	helps eliminate those wide spaces by replacing them with "regular" spaces.
+	'''
+	return ' '.join("`{}`".format(word) for word in text.split(' '))
+
+
+def map_country_code_to_flag(country_code: str) -> str:
+	flag_map = {
+		'FRA': '🇪🇺', 'FR': '🇪🇺', 'USA': '🇺🇸', 'EU': '🇪🇺',
+		'RUS': '🇷🇺', 'CHN': '🇨🇳', 'IND': '🇮🇳', 'JPN': '🇯🇵',
+		'IRN': '🇮🇷', 'NZL': '🇳🇿', 'GUF': '🇬🇫', 'UNK': '🏳'
+	}
+
+	return flag_map[country_code] if country_code in flag_map.keys() else '🏳'
+
+
 def timestamp_to_unix(timestamp: str) -> int:
-	'''Summary
+	''' Summary
 	Parses a LL2 timestamp from its format into a unix timestamp,
 	i.e. seconds since the unix epoch. 
 	'''
@@ -44,7 +62,7 @@ def timestamp_to_unix(timestamp: str) -> int:
 
 
 def time_delta_to_legible_eta(time_delta: int) -> str:
-	'''Summary
+	''' Summary
 	This is a tiny helper function, used to convert integer time deltas
 	(i.e. second deltas) to a legible ETA, where the largest unit of time
 	is measured in days.
