071dac9832368ab7f681b5d0f900ccabb9d2fef3
499602D2 <46869890+499602D2@users.noreply.github.com>
2020-10-23 23:25:03 +0300

    Add docstrings
    Added a complete module level docstring, added complete docstrings to all functions.
    
    - Added anonymize_id
    
    - Renamed shorten_monospaced_text to short_monospaced_text, as the latter descibes the functionality of the function more accurately.
    
    - Expand time_delta_to_legible_eta to return only seconds in the case the time period is less than a minute. If time delta is 0, return "just now".

--- tg-launchbot/utils.py
+++ tg-launchbot/utils.py
@@ -1,9 +1,54 @@
+'''
+utils.py includes some common, tiny helper functions used throughout the program.
+Including these separately in each file would be ugly, so they're here in their
+own little place.
+
+Classes:
+	None
+
+Functions:
+	anonymize_id(chat: str) -> str
+	reconstruct_link_for_markdown(link: str) -> str
+	reconstruct_message_for_markdown(message: str) -> str
+	short_monospaced_text(text: str) -> str
+	map_country_code_to_flag(country_code: str) -> str
+	timestamp_to_unix(timestamp: str) -> int
+	time_delta_to_legible_eta(time_delta: int) -> str
+
+Misc variables:
+	None
+'''
+
+
 import datetime
 
+from hashlib import sha1
+
+
+def anonymize_id(chat: str) -> str:
+	'''
+	For pseudo-anonymizing chat IDs, a truncated, unsalted SHA-1 hash
+	is returned for use in logging.
+
+	Keyword arguments:
+		chat (str): chat ID to anonymize
+
+	Returns:
+		chat (str): the anonymized chat ID
+	'''
+	return sha1(str(chat).encode('utf-8')).hexdigest()[0:6]
+
+
 def reconstruct_link_for_markdown(link: str) -> str:
-	''' Summary
+	'''
 	Telegram's MarkdownV2 requires some special handling, so
 	parse the link here into a compatible format.
+
+	Keyword arguments:
+		link (str): link to reconstruct for Markdown
+
+	Returns:
+		link_reconstruct (str): the reconstructed link
 	'''
 	link_reconstruct, char_set = '', {')', '\\'}
 	for char in link:
@@ -16,9 +61,15 @@ def reconstruct_link_for_markdown(link: str) -> str:
 
 
 def reconstruct_message_for_markdown(message: str) -> str:
-	''' Summary
+	'''
 	Performs effectively the same functions as reconstruct_link, but
 	is intended to be used for message body text.
+
+	Keyword arguments:
+		message (str): message to escape for Markdown
+
+	Returns:
+		message_reconstruct (str): the escaped message
 	'''
 	message_reconstruct = ''
 	char_set = {'[', ']', '(', ')', '~', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'}
@@ -31,17 +82,31 @@ def reconstruct_message_for_markdown(message: str) -> str:
 	return message_reconstruct
 
 
-def shorten_monospaced_text(text: str) -> str:
-	''' Summary
+def short_monospaced_text(text: str) -> str:
+	'''
 	Telegram has extremely wide spaces for the monospaced font. This function
 	helps eliminate those wide spaces by replacing them with "regular" spaces.
+
+	Keyword arguments:
+		text (str): text to monospace in a shortened format
+
+	Returns:
+		text (str): monospaced text
 	'''
 	return ' '.join("`{}`".format(word) for word in text.split(' '))
 
 
 def map_country_code_to_flag(country_code: str) -> str:
-	''' Summary
+	'''
 	Maps a country code to a corresponding emoji flag: truly modern.
+	The functions returns a blank, white flag if the country code
+	doesn't exist in the flag_map dictionary.
+
+	Keyword arguments:
+		country_code (str): country code to return the flag for
+
+	Returns:
+		emoji_flag (str): the flag for the country_code
 	'''
 	flag_map = {
 		'FRA': '🇪🇺', 'FR': '🇪🇺', 'USA': '🇺🇸', 'EU': '🇪🇺',
@@ -53,9 +118,15 @@ def map_country_code_to_flag(country_code: str) -> str:
 
 
 def timestamp_to_unix(timestamp: str) -> int:
-	''' Summary
+	'''
 	Parses a LL2 timestamp from its format into a unix timestamp,
 	i.e. seconds since the unix epoch. 
+
+	Keyword arguments:
+		timestamp (str): timestamp in the format used by the LL2 API
+
+	Returns:
+		unix_timestamp (int): unix timestamp corresponding to the above timestamp
 	'''
 	# convert to a datetime object from the custom format, ex. 2020-10-18T12:25:00Z
 	utc_dt = datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S%fZ')
@@ -65,10 +136,16 @@ def timestamp_to_unix(timestamp: str) -> int:
 
 
 def time_delta_to_legible_eta(time_delta: int) -> str:
-	''' Summary
+	'''
 	This is a tiny helper function, used to convert integer time deltas
 	(i.e. second deltas) to a legible ETA, where the largest unit of time
 	is measured in days.
+
+	Keyword arguments:
+		time_delta (int): time delta in seconds to convert
+
+	Returns:
+		pretty_eta (str): the prettily formatted, readable ETA string
 	'''
 	# convert time delta to a semi-redable format: {days, hh:mm:ss}
 	eta_str = "{}".format(str(datetime.timedelta(seconds=time_delta)))
@@ -82,13 +159,21 @@ def time_delta_to_legible_eta(time_delta: int) -> str:
 		if hours > 1:
 			pretty_eta += 's'
 	else:
+		# split eta_string into hours, minutes, and seconds -> convert to integers
 		hhmmss_split = eta_str.split(':')
 		hours, mins, secs = int(hhmmss_split[0]), int(hhmmss_split[1]), int(hhmmss_split[2])
+
 		if hours > 0:
 			pretty_eta = f'{hours} hour{"s" if hours > 1 else ""}'
 			pretty_eta += f', {mins} minute{"s" if mins != 1 else ""}'
 		else:
-			pretty_eta = f'{mins} minute{"s" if mins > 1 else ""}'
-			pretty_eta += f', {secs} second{"s" if secs != 1 else ""}'
+			if mins > 0:
+				pretty_eta = f'{mins} minute{"s" if mins > 1 else ""}'
+				pretty_eta += f', {secs} second{"s" if secs != 1 else ""}'
+			else:
+				if secs > 0:
+					pretty_eta = f'{secs} second{"s" if secs != 1 else ""}'
+				else:
+					pretta_eta = 'just now'
 
 	return pretty_eta
