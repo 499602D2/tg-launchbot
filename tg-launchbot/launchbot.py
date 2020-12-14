5157bd48317487cf04220de75e96c0a6c04786cb
499602D2 <46869890+499602D2@users.noreply.github.com>
2020-12-14 12:55:32 +0200

    Bump version

--- tg-launchbot/launchbot.py
+++ tg-launchbot/launchbot.py
@@ -2760,7 +2760,7 @@ if __name__ == '__main__':
 	global DATA_DIR, STARTUP_TIME
 
 	# current version, set DATA_DIR
-	VERSION = '1.7.5'
+	VERSION = '1.7.6'
 	DATA_DIR = 'launchbot'
 
 	# log startup time
