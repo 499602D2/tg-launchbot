12e42fbf4d3a49a01759dc2648ab5fd659b7afb5
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-04-08 01:58:01 +0300

    Add to-do methods
    Add method for adding a user to the queue, and for loading the user's time zone

--- users/users.go
+++ users/users.go
@@ -1,15 +1,49 @@
 package users
 
+import (
+	"sync"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
 type User struct {
 	Platform string // Discord=dg, Telegram=tb, Email=email
 	Id       int64
-	TimeZone string
+	TimeZone *time.Location
 }
 
 /* Extends the User type by creating a list of users.
 This can be userful for e.g. sending notifications to one platform. */
 type UserList struct {
-	Platform  string
-	Users     *[]User
-	UserCount int
+	Platform string
+	Users    []*User
+	Mutex    sync.Mutex
+}
+
+func (user *User) LoadTimeZone() {
+	// Loads the user's time zone information from cache/disk
+	tz, err := time.LoadLocation("UTC")
+	if err != nil {
+		log.Error().Err(err).Msg("Error loading time zone for user")
+		return
+	}
+
+	// Set the loaded time zone
+	user.TimeZone = tz
+	log.Warn().Msg("Proper time zone loading not implemented!")
+}
+
+/* Adds a single user to a UserList and adds a time zone if required */
+func (userList *UserList) Add(user User, addTimeZone bool) {
+	userList.Mutex.Lock()
+
+	if addTimeZone {
+		user.LoadTimeZone()
+	}
+
+	// Add user to the list
+	userList.Users = append(userList.Users, &user)
+
+	userList.Mutex.Unlock()
 }
