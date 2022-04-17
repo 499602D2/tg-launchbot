9cc528674a19cb21612cba3b3dd91526b68746c8
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-04-17 20:33:22 +0300

    Update stats.go

--- statistics/stats.go
+++ statistics/stats.go
@@ -4,4 +4,5 @@ package statistics
 - add a "next API update in..." field
 - add a next notification type, and the launch it's for
 - add a trailing-day set of stats (commands, notifications, callbacks)
+- add ratelimits enforced statistic (if limiter returns false)
 */
