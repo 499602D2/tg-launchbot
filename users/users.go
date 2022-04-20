1fb185786b727aae8fa4a2d7bbf94f28fcf8cc62
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-04-21 01:48:38 +0300

    Add UTC-offset field

--- users/users.go
+++ users/users.go
@@ -1,6 +1,7 @@
 package users
 
 import (
+	"fmt"
 	"sync"
 	"time"
 
@@ -10,7 +11,12 @@ import (
 type User struct {
 	Platform string // Discord=dg, Telegram=tb, Email=email
 	Id       int64
-	TimeZone *time.Location
+	Time     UserTime
+}
+
+type UserTime struct {
+	Location  *time.Location // User's time zone for the Time module
+	UtcOffset string         // A legible UTC offset string, e.g. "UTC+5"
 }
 
 /*
@@ -26,15 +32,37 @@ type UserList struct {
 /* Loads the user's time zone information from cache/disk */
 func (user *User) LoadTimeZone() {
 	// TODO do properly
-	tz, err := time.LoadLocation("UTC")
+	tz, err := time.LoadLocation("Europe/Helsinki")
 
 	if err != nil {
 		log.Error().Err(err).Msg("Error loading time zone for user")
 		return
 	}
 
-	// Set the loaded time zone
-	user.TimeZone = tz
+	// Create time field for user
+	user.Time = UserTime{
+		Location:  tz,
+		UtcOffset: "",
+	}
+
+	// Get offset from user's current time
+	userTime := time.Now().In(tz)
+	_, offset := userTime.Zone()
+
+	// Add a plus if the offset is positive
+	user.Time.UtcOffset += map[bool]string{true: "+", false: ""}[offset >= 0]
+
+	if offset%3600 == 0 {
+		// If divisible by 3600, the offset is an integer hour
+		user.Time.UtcOffset += fmt.Sprintf("%d", offset/3600)
+	} else {
+		// Extract whole hours from the second offset
+		hours := (offset - (offset % 3600)) / 3600
+		mins := (offset % 3600) / 60
+
+		user.Time.UtcOffset += fmt.Sprintf("%d:%2d", hours, mins)
+	}
+
 	log.Warn().Msg("Proper time zone loading not implemented!")
 }
 
