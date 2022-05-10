7d9ce6ef2e395513da1ea44551c7a1e505889cd6
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-05-10 12:19:45 +0300

    Add method to print out statistics

--- stats/stats.go
+++ stats/stats.go
@@ -1,6 +1,12 @@
 package stats
 
-import "time"
+import (
+	"fmt"
+	"launchbot/utils"
+	"time"
+
+	"github.com/dustin/go-humanize"
+)
 
 /* TODO
 - add a "next API update in..." field
@@ -11,12 +17,34 @@ import "time"
 
 // Global statistics, per-platform
 type Statistics struct {
-	Platform      string `gorm:"primaryKey;uniqueIndex"`
-	Notifications int64
-	Commands      int64
-	ApiRequests   int64
-	LastApiUpdate time.Time
-	UpdatedAt     time.Time
+	RunningVersion      string `gorm:"-:all"`
+	Platform            string `gorm:"primaryKey;uniqueIndex"`
+	Notifications       int64
+	Commands            int64
+	ApiRequests         int64
+	RequestsRatelimited int64
+	LastApiUpdate       time.Time
+	NextApiUpdate       time.Time
+	StartedAt           time.Time
+}
+
+type ScheduleCmdStats struct {
+	CommandCalls        int64
+	RefreschCallbacks   int64
+	ModeSwitchCallbacks int64
+}
+
+type NextCmdStats struct {
+	CommandCalls      int64
+	RefreshCallbacks  int64
+	NextCallbacks     int64
+	PreviousCallbacks int64
+	ReturnCallbacks   int64
+}
+
+type StatsCmdStats struct {
+	CommandCalls     int64
+	RefreshCallbacks int64
 }
 
 // Embedded chat-level statistics
@@ -25,4 +53,36 @@ type User struct {
 	SentCommands          int64
 	MemberCount           int64
 	SubscribedSince       int64
+}
+
+func (stats *Statistics) String() string {
+	//cpuPerc, err := cpu.Percent(time.Minute, false)
+	//if err != nil {
+	//	log.Error().Err(err).Msg("Getting CPU usage failed")
+	//	cpuPerc = []float64{0.00}
+	//}
+
+	// Get subscriber count from disk
+	subscribers := 0
+
+	sinceStartup := humanize.Time(stats.StartedAt)
+
+	text := fmt.Sprintf(
+		"📊 *LaunchBot global statistics*\n"+
+			"Notification delivered: %d\n"+
+			"Active subscribers: %d\n"+
+			"Commands parsed: %d\n\n"+
+
+			"🌍 *Backend information*\n"+
+			"Bot started %s\n"+
+			"LaunchBot %s",
+
+		stats.Notifications,
+		subscribers,
+		stats.Commands,
+		sinceStartup,
+		stats.RunningVersion,
+	)
+
+	return utils.PrepareInputForMarkdown(text, "text")
 }
