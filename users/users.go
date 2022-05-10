cda902e3622a73a1288591c66501259ec83d78bd
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-05-11 01:07:12 +0300

    User functions to support notification settings
    Also sets, by default, 12-hour and 1-hour notification flags to 0. Also adds a flag for postpone notifications

--- users/users.go
+++ users/users.go
@@ -3,6 +3,8 @@ package users
 import (
 	"fmt"
 	"launchbot/stats"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -15,14 +17,15 @@ type User struct {
 	Platform             string   `gorm:"primaryKey"`
 	Locale               string   // E.g. "Europe/Berlin"
 	Time                 UserTime `gorm:"-:all"`
-	Enabled24h           bool     `gorm:"index:enabled;index:disabled"`
-	Enabled12h           bool     `gorm:"index:enabled;index:disabled"`
-	Enabled1h            bool     `gorm:"index:enabled;index:disabled"`
-	Enabled5min          bool     `gorm:"index:enabled;index:disabled"`
+	Enabled24h           bool     `gorm:"index:enabled;index:disabled;default:1"`
+	Enabled12h           bool     `gorm:"index:enabled;index:disabled;default:0"`
+	Enabled1h            bool     `gorm:"index:enabled;index:disabled;default:0"`
+	Enabled5min          bool     `gorm:"index:enabled;index:disabled;default:1"`
+	EnabledPostpone      bool     `gorm:"index:enabled;index:disabled;default:1"`
 	SubscribedAll        bool     `gorm:"index:enabled;index:disabled"`
+	SubscribedTo         string   // List of comma-separated LSP IDs
+	UnsubscribedFrom     string   // List of comma-separated LSP IDs
 	SubscribedNewsletter bool
-	SubscribedTo         string     // List of LSP IDs
-	UnsubscribedFrom     string     // List of LSP IDs
 	Stats                stats.User `gorm:"embedded"`
 	CreatedAt            time.Time
 	UpdatedAt            time.Time
@@ -47,6 +50,138 @@ type UserList struct {
 	Platform string
 	Users    []*User
 	Mutex    sync.Mutex
+}
+
+// Returns a list of integers for all enabled and disabled launch provider IDs
+func (user *User) GetNotificationStates() ([]int, []int) {
+	var (
+		enabledIds  []int
+		disabledIds []int
+		intId       int
+	)
+
+	if user.SubscribedTo != "" {
+		for _, strId := range strings.Split(user.SubscribedTo, ",") {
+			intId, _ = strconv.Atoi(strId)
+			enabledIds = append(enabledIds, intId)
+		}
+	}
+
+	if user.UnsubscribedFrom != "" {
+		for _, strId := range strings.Split(user.UnsubscribedFrom, ",") {
+			intId, _ = strconv.Atoi(strId)
+			disabledIds = append(disabledIds, intId)
+		}
+	}
+
+	return enabledIds, disabledIds
+}
+
+func (user *User) GetNotificationStateMap() map[int]bool {
+	enabled, disabled := user.GetNotificationStates()
+	stateMap := map[int]bool{}
+
+	for _, id := range enabled {
+		stateMap[id] = true
+	}
+
+	for _, id := range disabled {
+		stateMap[id] = false
+	}
+
+	return stateMap
+}
+
+func (user *User) GetNotificationStatusById(id int) bool {
+	// If user has subscribed to this ID, return true
+	if strings.Contains(user.SubscribedTo, fmt.Sprint(id)) {
+		return true
+	}
+
+	// If user has the all-flag flipped, and has not unsubscribed from this ID, return true
+	if user.SubscribedAll && !strings.Contains(user.UnsubscribedFrom, fmt.Sprint(id)) {
+		return true
+	}
+
+	// Otherwise, return false
+	return false
+}
+
+func (user *User) SetAllFlag(newState bool) {
+	// Flip the flag
+	user.SubscribedAll = newState
+
+	// Remove any manually set IDs
+	user.SubscribedTo = ""
+	user.UnsubscribedFrom = ""
+}
+
+func (user *User) SetNotificationTimeFlag(flagName string, newState bool) {
+	switch flagName {
+	case "24h":
+		user.Enabled24h = newState
+	case "12h":
+		user.Enabled12h = newState
+	case "1h":
+		user.Enabled1h = newState
+	case "5min":
+		user.Enabled5min = newState
+	case "postpone":
+		user.EnabledPostpone = newState
+	}
+}
+
+func (user *User) AllNotificationTimesEnabled() bool {
+	if user.Enabled24h && user.Enabled12h && user.Enabled1h && user.Enabled5min && user.EnabledPostpone {
+		return true
+	}
+
+	return false
+}
+
+func (user *User) ToggleIdSubscription(ids []string, newState bool) {
+	// Load notification states as a map of id:bool for easy updates
+	stateMap := user.GetNotificationStateMap()
+
+	// Convert to an int, set in map
+	for _, id := range ids {
+		idInt, _ := strconv.Atoi(id)
+
+		if newState == true && user.SubscribedAll || newState == false && !user.SubscribedAll {
+			delete(stateMap, idInt)
+		} else {
+			stateMap[idInt] = newState
+		}
+	}
+
+	// Save updated map
+	user.SaveFromMap(stateMap)
+}
+
+func (user *User) SaveFromMap(stateMap map[int]bool) {
+	enabled := []string{}
+	disabled := []string{}
+
+	for id, state := range stateMap {
+		if state == true {
+			enabled = append(enabled, fmt.Sprint(id))
+		} else {
+			disabled = append(disabled, fmt.Sprint(id))
+		}
+	}
+
+	// Join slices and assign
+	if len(enabled) != 0 {
+		user.SubscribedTo = strings.Join(enabled, ",")
+	} else {
+		user.SubscribedTo = ""
+	}
+
+	if len(disabled) != 0 {
+		user.UnsubscribedFrom = strings.Join(disabled, ",")
+	} else {
+		user.UnsubscribedFrom = ""
+	}
 }
 
 // Sets the user's user.Time field. Called when user is loaded from or saved to DB.
