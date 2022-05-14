12e70de79787365ae321021bb97967b49e400e66
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-05-15 01:34:00 +0300

    Generalize sender
    + Works with both msg deletions and sends now
    + Save sent notification IDs
    + Delete old notifications

--- bots/sender.go
+++ bots/sender.go
@@ -1,12 +1,14 @@
 package bots
 
 import (
+	"fmt"
 	"launchbot/sendables"
 	"launchbot/users"
 	"strconv"
 	"sync"
 	"time"
 
+	"github.com/hako/durafmt"
 	"github.com/rs/zerolog/log"
 	uuid "github.com/satori/go.uuid"
 	tb "gopkg.in/telebot.v3"
@@ -26,6 +28,7 @@ func (queue *Queue) Enqueue(sendable *sendables.Sendable, tg *TelegramBot, highP
 
 	// Calculate size and set token count
 	sendableSize := sendable.PerceivedByteSize()
+
 	if sendableSize >= 512 && !highPriority {
 		sendable.Tokens = 6
 		log.Debug().Msgf("Reserved %d token(s) for sendable, size=%d", sendable.Tokens, sendableSize)
@@ -60,10 +63,9 @@ func highPrioritySender(tg *TelegramBot, message *sendables.Message, user *users
 		text = sendables.SetTime(*text, user, message.RefTime, true)
 	}
 
-	// TODO store integer-id
 	id, _ := strconv.Atoi(user.Id)
 
-	// TODO: use sendable.Send()
+	// FUTURE: use sendable.Send()
 	sent, err := tg.Bot.Send(tb.ChatID(id),
 		*text, &message.SendOptions,
 	)
@@ -90,7 +92,7 @@ func clearPriorityQueue(tg *TelegramBot) {
 
 	// TODO: sort before looping over (according to priority)
 	for _, prioritySendable := range tg.HighPriority.Queue {
-		for _, priorityUser := range prioritySendable.Recipients.Users {
+		for _, priorityUser := range prioritySendable.Recipients {
 			// Loop over users, send high-priority message
 			highPrioritySender(tg, prioritySendable.Message, priorityUser)
 		}
@@ -106,117 +108,176 @@ func clearPriorityQueue(tg *TelegramBot) {
 	tg.HighPriority.Mutex.Unlock()
 }
 
+func DeleteMessage(tg *TelegramBot, sendable *sendables.Sendable, user *users.User) {
+	// Load ID pair
+	msgId, ok := sendable.MessageIDs[user.Id]
+
+	if !ok {
+		log.Warn().Msgf("Unable to find message ID during deletion for user=%s", user.Id)
+		return
+	}
+
+	// Convert chat ID to integer
+	chatId, err := strconv.Atoi(user.Id)
+
+	if err != nil {
+		log.Error().Err(err).Msgf("Forming chat ID failed while removing sent messages")
+		return
+	}
+
+	// Convert message ID to an integer
+	messageId, err := strconv.Atoi(msgId)
+
+	if err != nil {
+		log.Error().Err(err).Msgf("Forming message ID failed while removing sent messages")
+		return
+	}
+
+	// Build tb.Message, and delete it
+	err = tg.Bot.Delete(&tb.Message{ID: messageId, Chat: &tb.Chat{ID: int64(chatId)}})
+
+	if err != nil {
+		log.Error().Err(err).Msgf("Deleting message %s:%s failed", user.Id, msgId)
+	}
+}
+
+func SendNotification(tg *TelegramBot, sendable *sendables.Sendable, user *users.User) (string, bool) {
+	// Convert id to an integer
+	id, _ := strconv.Atoi(user.Id)
+
+	var text *string
+	if sendable.Message.AddUserTime {
+		text = sendables.SetTime(*sendable.Message.TextContent, user, sendable.Message.RefTime, true)
+	} else {
+		text = sendable.Message.TextContent
+	}
+
+	// Send message
+	sent, err := tg.Bot.Send(tb.ChatID(id), *text, &sendable.Message.SendOptions)
+
+	if err != nil {
+		if !handleSendError(sent, err, tg) {
+			log.Warn().Msg("Non-recoverable error in sender, continuing loop")
+			return "", false
+		} else {
+			// TODO Error is recoverable: try sending again SendNotification(...)
+			log.Warn().Msg("NOT IMPLEMENTED: message re-try after recoverable error (e.g. timeout)")
+		}
+	}
+
+	// On success, return a string in the form of 'user_id:msg_id', and a bool indicating success
+	return fmt.Sprintf("%s:%d", user.Id, sent.ID), true
+}
+
 // TelegramSender is a daemon-like function that listens to the notification
 // and priority queues for incoming messages and notifications.
-
-// TODO: alternatively use a job queue + dispatcher (V3.1+)
-// - should feature priority tags
-
-// Alternatively: implement Sendables so that they are unwrapped into distinct
-// objects, that can then be queued. Then, implement a simple, linked priority-
-// weighed queue and a dequeuer + all the related queue/dequeue/insert methods.
-
-// This could replace the priority queue, as the head of the queue would be the
-// one that's always deleted.
-
-// - Mutexes? Constant locking + unlocking (which may be fine)
 func TelegramSender(tg *TelegramBot) {
-	const (
-		priorityQueueClearInterval = 3
-	)
+	const priorityQueueClearInterval = 10
+	var processStartTime time.Time
 
 	for {
 		// Check notification queue
 		if len(tg.Queue.Sendables) != 0 {
+			// Lock queue while processing
 			tg.Queue.Mutex.Lock()
 
 			for hash, sendable := range tg.Queue.Sendables {
-				// Keep track of sent IDs
-				sentIds := make(map[users.User]int)
+				// Processing time
+				processStartTime = time.Now()
 
-				// Keep pointer, simplifies handling if time needs to be set in message
-				defaultText := sendable.Message.TextContent
-				var text *string
+				// Keep track of sent IDs
+				sentIds := []string{}
 
 				// Calculate how many tokens each message requires
 				if sendable.PerceivedByteSize() >= 512 {
 					log.Warn().Msgf("Sendable is more than 512 bytes long, taking 6 tokens per send")
 				}
 
-				log.Info().Msgf("Sending sendable with hash=%s", hash)
+				log.Info().Msgf("Processing sendable with hash=%s, type=%s", hash, sendable.Type)
 
-				var i uint32
-				for i, user := range sendable.Recipients.Users {
+				// Loop over users this sendable is meant for
+				for i, user := range sendable.Recipients {
 					// Rate-limiter: check if we have tokens to proceed
 					tg.Spam.GlobalLimiter(sendable.Tokens)
 
-					// If message needs to have its time set properly, do it now
-					if sendable.Message.AddUserTime {
-						text = sendables.SetTime(*defaultText, user, sendable.Message.RefTime, true)
-					} else {
-						text = defaultText
+					// Switch-case the sendable's type
+					switch sendable.Type {
+					case "notification":
+						log.Debug().Msgf("Sending notification to user=%s", user.Id)
+						sentIdPair, success := SendNotification(tg, sendable, user)
+
+						if success {
+							sentIds = append(sentIds, sentIdPair)
+						}
+					case "delete":
+						DeleteMessage(tg, sendable, user)
 					}
 
-					// TODO: use sendable.Send()
-					// Use the tb.Sendable interface?
-					// https://pkg.go.dev/gopkg.in/telebot.v3@v3.0.0?utm_source=gopls#Sendable
-					id, _ := strconv.Atoi(user.Id)
-					sent, err := tg.Bot.Send(tb.ChatID(id), *text, &sendable.Message.SendOptions)
+					/* Periodically, during long sends, check if the TelegramBot.PriorityQueued is set.
+					This flag is enabled if there is one, or more, enqueued high-priority messages
+					in the high-priority queue. Vary the priorityQueueClearInterval variable to tune
+					how often to check for pending messages.
 
-					if err != nil {
-						if !handleSendError(sent, err, tg) {
-							// If error is non-recoverable, continue the loop
-							log.Warn().Msg("Non-recoverable error in sender, continuing loop")
-							delete(tg.Queue.Sendables, hash)
-							continue
-						} else {
-							// Error is recoverable: try sending again
-							// TODO re-try (TelegramMessageSender(...))
-							log.Warn().Msg("NOT IMPLEMENTED: message re-try after recoverable error (e.g. timeout)")
-						}
-					} else {
-						// Successfully sent; store sent notification's ID for later use
-						delete(tg.Queue.Sendables, hash)
-						if sendable.Type == "notification" {
-							sentIds[*user] = sent.ID
-						}
-					}
-
-					/*
-						Periodically, during long sends, check if the TelegramBot.PriorityQueued is set.
-						This flag is enabled if there is one, or more, enqueued high-priority messages
-						in the high-priority queue. Vary the priorityQueueClearInterval variable to tune
-						how often to check for pending messages.
-
-						The justification for this is the fact that the main queue's mutex is locked when
-						the sending process is started, and could be locked for minutes. This alleviates
-						the issue of messages sitting in the queue for ages, sacrificing the send time of
-						mass-notifications for timely responses to e.g. callback queries and commands.
-					*/
+					The justification for this is the fact that the main queue's mutex is locked when
+					the sending process is started, and could be locked for minutes. This alleviates
+					the issue of messages sitting in the queue for ages, sacrificing the send time of
+					mass-notifications and mass-removals for timely responses to commands. */
 					if (tg.HighPriority.HasItemsInQueue) && (i%priorityQueueClearInterval == 0) {
 						log.Debug().Msg("High-priority messages in queue during long send")
 						clearPriorityQueue(tg)
 					}
 				}
 
-				// db.SaveSentNotificationIds()
-				if sendable.Type == "notification" {
-					// TODO save notification IDs to database
+				// Log time spent processing
+				timeSpent := durafmt.Parse(time.Since(processStartTime)).LimitFirstN(1)
+
+				// All done: do post-processing
+				switch sendable.Type {
+				case "notification":
+					// Send done; log
+					log.Info().Msgf("Sent %d notification(s) for sendable=%s in %s", len(sentIds), hash, timeSpent)
 
 					// Save statistics
 					tg.Stats.Notifications += len(sentIds)
+
+					// Load launch from cache so we can save the IDs
+					launch, err := tg.Cache.FindLaunchById(sendable.LaunchId)
+
+					if err != nil {
+						log.Error().Err(err).Msgf("Unable to find launch while saving sent message IDs")
+					}
+
+					// If launch has previously sent notifications, delete them
+					if launch.SentNotificationIds != "" {
+						log.Debug().Msgf("Launch has previously sent notifications, removing...")
+
+						// Load IDs of previously sent notifications
+						previouslySentIds := launch.LoadSentNotificationIdMap()
+
+						// Create a sendable for removing mass-notifications
+						deletionSendable := sendables.SendableForMessageRemoval(sendable, previouslySentIds)
+
+						// Enqueue the sendable for removing the old notifications
+						go tg.Queue.Enqueue(deletionSendable, tg, false)
+					}
+
+					// Save the IDs of the sent notifications
+					launch.SaveSentNotificationIds(sentIds, tg.Db)
+
+				case "delete":
+					log.Info().Msgf("Processed %d message removals in %s", len(sendable.MessageIDs), timeSpent)
 				}
 
-				// Send done, log
-				log.Info().Msgf("Sent %d notification(s) for sendable=%s", i+1, hash)
+				// Delete sendable from the queue
+				delete(tg.Queue.Sendables, hash)
 			}
 
+			// Unlock mutex after each sendable
 			tg.Queue.Mutex.Unlock()
 		}
 
 		// Check if priority queue is populated (and skip sleeping if one entry)
 		if tg.HighPriority.HasItemsInQueue {
-			//log.Debug().Msg("High-priority messages in queue")
 			clearPriorityQueue(tg)
 		}
 
