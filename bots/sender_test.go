9256973a35ebe98559fa478c55a722fb6d467553
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-05-15 15:22:07 +0300

    Update sender_test.go

--- bots/sender_test.go
+++ bots/sender_test.go
@@ -11,14 +11,14 @@ import (
 func TestSetTime(t *testing.T) {
 	txt := "$USERTIME"
 	msg := sendables.Message{
-		TextContent: &txt, AddUserTime: true, RefTime: time.Now().Unix(),
+		TextContent: txt, AddUserTime: true, RefTime: time.Now().Unix(),
 	}
 
 	// Positive offset
 	user1 := users.User{}
 	user1.SetTimeZone()
-	newText := sendables.SetTime(*msg.TextContent, &user1, msg.RefTime, false)
-	fmt.Println(*newText)
+	newText := sendables.SetTime(msg.TextContent, &user1, msg.RefTime, false, false)
+	fmt.Println(newText)
 
 	// Local offset
 	// "Europe/Helsinki"
