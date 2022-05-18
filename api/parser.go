e67134dced4ecfb3b6731d4701520f51b35129c0
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-05-18 20:22:20 +0300

    Parse launches concurrently
    Results in an over 300 % speed-up compared to doing it synchronously. The processing was taking unacceptably long after the natural language parser was introduced, as in over 10 seconds per one launch update. This cuts processing time down to 3 seconds, with zero fancy optimizations: just a mutex and a whole bunch of go-routines.

--- api/parser.go
+++ api/parser.go
@@ -4,6 +4,7 @@ import (
 	"launchbot/db"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/jdkato/prose/v2"
@@ -31,7 +32,8 @@ func GetHighestPriorityVideoLink(links []db.ContentURL) *db.ContentURL {
 	return &links[highestPriorityIndex]
 }
 
-/* Checks if the NET of a launch slipped from one update to another. */
+// Checks if the NET of a launch slipped from one update to another.
+// Returns a bool indicating if this happened, and a Postpone{} characterizing the slip
 func netSlipped(cache *db.Cache, freshLaunch *db.Launch) (bool, db.Postpone) {
 	/* Launch not found in cache, check on disk
 
@@ -40,7 +42,12 @@ func netSlipped(cache *db.Cache, freshLaunch *db.Launch) (bool, db.Postpone) {
 	cacheLaunch, ok := cache.LaunchMap[freshLaunch.Id]
 
 	if !ok {
-		log.Debug().Msgf("Launch with id=%s not found in cache", freshLaunch.Id)
+		if time.Now().Unix() > freshLaunch.NETUnix {
+			log.Debug().Msgf("(OK) Launch is in the past, and not found in cache slug=%s", freshLaunch.Slug)
+		} else {
+			log.Warn().Msgf("Launch with slug=%s not found in cache, but NET in the future", freshLaunch.Slug)
+		}
+
 		return false, db.Postpone{}
 	}
 
@@ -64,73 +71,94 @@ func netSlipped(cache *db.Cache, freshLaunch *db.Launch) (bool, db.Postpone) {
 	return false, db.Postpone{}
 }
 
-/* Parses the LL2 launch update. */
-func parseLaunchUpdate(cache *db.Cache, update *db.LaunchUpdate) ([]*db.Launch, map[*db.Launch]db.Postpone, error) {
-	var utcTime time.Time
-	var err error
+// Process a single launch; function is run concurrently.
+func processLaunch(launch *db.Launch, launchUpdate *db.LaunchUpdate, idx int, cache *db.Cache, wg *sync.WaitGroup) {
+	// Parse the datetime string as RFC3339 into a time.Time object in UTC
+	utcTime, err := time.ParseInLocation(time.RFC3339, launch.NET, time.UTC)
 
-	postponedLaunches := map[*db.Launch]db.Postpone{}
+	if err != nil {
+		log.Error().Err(err).Msg("Error parsing RFC3339 launch time")
+	}
 
-	// Loop over launches and do any required operations
-	for _, launch := range update.Launches {
-		// Parse the datetime string as RFC3339 into a time.Time object in UTC
-		utcTime, err = time.ParseInLocation(time.RFC3339, launch.NET, time.UTC)
+	// Convert to unix time, store
+	launch.NETUnix = time.Time.Unix(utcTime)
 
-		if err != nil {
-			log.Error().Err(err).Msg("Error parsing RFC3339 launch time")
-		}
+	// Set launched status
+	// 3: success, 4: failure, 6: in-flight, 7: partial failure
+	switch launch.Status.Id {
+	case 3, 4, 6, 7:
+		launch.Launched = true
+	}
 
-		// Convert to unix time, store
-		launch.NETUnix = time.Time.Unix(utcTime)
+	// Shorten description, by keeping the first two sentences
+	// Extremely slow, approx. 300 ms per launch.
+	document, err := prose.NewDocument(launch.Mission.Description)
+	sentences := make([]string, len(document.Sentences()))
 
-		// Set launched status
-		// 3: success, 4: failure, 6: in-flight, 7: partial failure
-		switch launch.Status.Id {
-		case 3, 4, 6, 7:
-			launch.Launched = true
-		}
-
-		// Shorten description, by keeping the first two sentences
-		document, err := prose.NewDocument(launch.Mission.Description)
-		sentences := []string{}
-
-		if err != nil {
-			log.Error().Err(err).Msgf("Processing description for launch=%s failed", launch.Id)
-		} else {
-			if len(document.Sentences()) > 2 {
-				// More than two sentences: move their text content to an array
-				for _, sentence := range document.Sentences() {
-					sentences = append(sentences, sentence.Text)
-				}
-
-				// Join first two sentences
-				launch.Mission.Description = strings.Join(sentences[:2], " ")
+	if err != nil {
+		log.Error().Err(err).Msgf("Processing description for launch=%s failed", launch.Id)
+	} else {
+		if len(document.Sentences()) > 2 {
+			// More than two sentences: move their text content to an array
+			for _, sentence := range document.Sentences() {
+				sentences = append(sentences, sentence.Text)
 			}
-		}
 
-		// Shorten launch pad names
-		launch.LaunchPad.Name = strings.ReplaceAll(launch.LaunchPad.Name, "Satellite Launch Center ", "SLC-")
-		launch.LaunchPad.Name = strings.ReplaceAll(launch.LaunchPad.Name, "Space Launch Complex ", "SLC-")
-		launch.LaunchPad.Name = strings.ReplaceAll(launch.LaunchPad.Name, "Launch Complex ", "LC-")
-
-		// Get the highest priority webcast URL
-		highestPriorityUrl := GetHighestPriorityVideoLink(launch.VidURL)
-		launch.WebcastLink = highestPriorityUrl.Url
-
-		// TODO If reused stage information, parse...
-
-		// If launch slipped enough to reset a notification state, save it
-		postponed, postponeStatus := netSlipped(cache, launch)
-
-		if postponed {
-			postponedLaunches[launch] = postponeStatus
+			// Join first two sentences
+			launch.Mission.Description = strings.Join(sentences[:2], " ")
 		}
 	}
+
+	// Shorten launch pad names
+	launch.LaunchPad.Name = strings.ReplaceAll(launch.LaunchPad.Name, "Satellite Launch Center ", "SLC-")
+	launch.LaunchPad.Name = strings.ReplaceAll(launch.LaunchPad.Name, "Space Launch Complex ", "SLC-")
+	launch.LaunchPad.Name = strings.ReplaceAll(launch.LaunchPad.Name, "Launch Complex ", "LC-")
+
+	// Get the highest priority webcast URL
+	highestPriorityUrl := GetHighestPriorityVideoLink(launch.VidURL)
+	launch.WebcastLink = highestPriorityUrl.Url
+
+	// TODO If reused stage information, parse...
+
+	// If launch slipped enough to reset a notification state, save it
+	postponed, postponeStatus := netSlipped(cache, launch)
+
+	// Lock mutex so we can save the launch
+	launchUpdate.Mutex.Lock()
+	defer launchUpdate.Mutex.Unlock()
+
+	if postponed {
+		// If launch was postponed, add it to the update
+		launchUpdate.Postponed[launch] = postponeStatus
+	}
+
+	// Update launch in launchUpdate (Mutex is locked so this is thread-safe)
+	launchUpdate.Launches[idx] = launch
+
+	// Worker done
+	wg.Done()
+}
+
+// Parses the LL2 launch update, returning the parsed launches and any launches that were postponed
+func parseLaunchUpdate(cache *db.Cache, update *db.LaunchUpdate) ([]*db.Launch, map[*db.Launch]db.Postpone, error) {
+	// Process launches concurrently
+	var wg sync.WaitGroup
+
+	// Add concurrent workers (results in a +300 % speed-up compared to synchronous)
+	wg.Add(len(update.Launches))
+
+	// Loop over launches and spawn go-routines
+	for idx, launch := range update.Launches {
+		go processLaunch(launch, update, idx, cache, &wg)
+	}
+
+	// Wait for all processes to finish
+	wg.Wait()
 
 	// Sort launches so they are ordered by NET
 	sort.Slice(update.Launches, func(i, j int) bool {
 		return update.Launches[i].NETUnix < update.Launches[j].NETUnix
 	})
 
-	return update.Launches, postponedLaunches, nil
+	return update.Launches, update.Postponed, nil
 }
