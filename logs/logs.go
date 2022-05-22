70bcaf1375e5e6d55d17bab94ae13cb077e6c1b4
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-05-22 21:33:06 +0300

    Add method to get size of log-file

--- logs/logs.go
+++ logs/logs.go
@@ -17,7 +17,7 @@ func SetupLogFile(logFolder string) *os.File {
 		_ = os.Mkdir(logPath, os.ModePerm)
 	}
 
-	logFilePath := filepath.Join(logPath, "bot.log")
+	logFilePath := filepath.Join(logPath, "launchbot-logs.log")
 	logf, err := os.OpenFile(logFilePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 
 	if err != nil {
@@ -25,4 +25,21 @@ func SetupLogFile(logFolder string) *os.File {
 	}
 
 	return logf
+}
+
+func GetLogSize(filepath string) int64 {
+	// Default filepath is logs/bot.log
+	if filepath == "" {
+		filepath = "logs/launchbot-logs.log"
+	}
+
+	fileInfo, err := os.Stat(filepath)
+
+	if err != nil {
+		log.Error().Err(err).Msg("Getting log-file stats failed")
+		return 0
+	}
+
+	// Set the size
+	return fileInfo.Size()
 }
