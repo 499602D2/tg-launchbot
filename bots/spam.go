489d3273872c705e55b4939c73d1b76808cd0f2a
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-05-30 22:33:14 +0300

    Use time.Duration directly, instead of seconds

--- bots/spam.go
+++ bots/spam.go
@@ -70,16 +70,16 @@ func (spam *Spam) UserLimiter(chat *users.User, stats *stats.Statistics, tokens 
 	err := spam.Chats[chat].Limiter.WaitN(context.Background(), tokens)
 
 	// Track enforced limits
-	duration := time.Since(start).Seconds()
+	duration := time.Since(start)
 	stats.LimitsEnforced++
 
 	// FUTURE track means with a fixed-length set of rate-limit durations (average same-index insertions)
 	if stats.LimitsEnforced == 1 {
-		stats.LimitsAverage = duration
+		stats.LimitsAverage = duration.Seconds()
 	} else {
 		/* If duration is lower than the average, the average drops. And, if
 		the duration is greater than the average, the average increases. */
-		stats.LimitsAverage = stats.LimitsAverage + (duration-stats.LimitsAverage)/float64(stats.LimitsEnforced)
+		stats.LimitsAverage = stats.LimitsAverage + (duration.Seconds()-stats.LimitsAverage)/float64(stats.LimitsEnforced)
 	}
 
 	if err != nil {
@@ -131,7 +131,7 @@ func (spam *Spam) PreHandler(interaction *Interaction, chat *users.User, stats *
 				}
 
 				if spam.VerboseLog {
-					log.Debug().Msgf("[pre-handler] Not allowing interaction=%s/%s in chat=%s",
+					log.Debug().Msgf("[Pre-handler] Not allowing interaction=%s/%s in chat=%s",
 						interaction.Name, interaction.CbData, chat.Id)
 				}
 
@@ -154,7 +154,7 @@ func (spam *Spam) PreHandler(interaction *Interaction, chat *users.User, stats *
 	spam.RunBothLimiters(chat, interaction.Tokens, stats)
 
 	if spam.VerboseLog {
-		log.Debug().Msgf("[pre-handler] Allowed interaction=%s/%s in chat=%s",
+		log.Debug().Msgf("[Pre-handler] Allowed interaction=%s/%s in chat=%s",
 			interaction.Name, interaction.CbData, chat.Id)
 	}
 
