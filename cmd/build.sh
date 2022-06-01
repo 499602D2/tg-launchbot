bf630f247907d1353483eb729bc726f1bb454e2c
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-06-01 23:34:45 +0300

    Add echos

--- cmd/build.sh
+++ cmd/build.sh
@@ -6,4 +6,10 @@ LDFLAGS=(
 	"-X 'main.GitSHA=$SHA'"
 )
 
-go build -x -o ../launchbot -ldflags="${LDFLAGS[*]}"
+echo "Current head at $SHA"
+
+echo "Building LaunchBot..."
+
+go build -o ../launchbot -ldflags="${LDFLAGS[*]}"
+
+echo "Build finished"
