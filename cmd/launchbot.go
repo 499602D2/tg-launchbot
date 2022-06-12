bb41a8ce2744e9bc8b628daef36d58c122ad902f
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-06-12 19:52:33 +0300

    Bump version

--- cmd/launchbot.go
+++ cmd/launchbot.go
@@ -22,7 +22,7 @@ import (
 // Injected at build-time
 var GitSHA = "0000000000"
 
-const version = "3.1.11"
+const version = "3.1.12"
 
 // Listens for incoming interrupt signals
 func signalListener(session *config.Session) {
