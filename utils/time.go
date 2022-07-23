fc14d978b72554508417f22199327ede5a7dc85f
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-07-23 18:15:23 +0300

    Minor code cleanup
    No functional changes

--- utils/time.go
+++ utils/time.go
@@ -28,35 +28,31 @@ func DateInUserLocation(refTime int64, userLocation *time.Location) string {
 
 // Return a user-friendly ETA string, e.g. "today", "tomorrow", or "in 5 days"
 func FriendlyETA(userNow time.Time, eta time.Duration) string {
-	var friendlyETA string
-
 	// See if eta + userNow is still the same day
 	if userNow.Add(eta).Day() == userNow.Day() {
 		// Same day: launch is today
-		friendlyETA = "today"
-	} else {
-		var daysUntil float64
-
-		// Remove seconds that are not contributing to a whole day.
-		// As in, TTL might be 1.25 days: extract the .25 days
-		mod := int64(eta.Seconds()) % (24 * 3600)
-
-		// If, even after adding the remainder, the day is still today, calculating days is simple
-		if time.Now().Add(time.Second*time.Duration(mod)).Day() == time.Now().Day() {
-			daysUntil = eta.Hours() / 24
-		} else {
-			// If the remained, e.g. .25 days, causes us to jump over to tomorrow, add a +1 to the days
-			daysUntil = eta.Hours()/24 + 1
-		}
-
-		if daysUntil < 2.0 {
-			// The case of the date being today has already been caught, therefore it's tomorrow
-			friendlyETA = "tomorrow"
-		} else {
-			// Otherwise, just count the days
-			friendlyETA = fmt.Sprintf("in %s", english.Plural(int(daysUntil), "day", "days"))
-		}
+		return "today"
 	}
 
-	return friendlyETA
+	// Remove seconds that are not contributing to a whole day.
+	// As in, TTL might be 1.25 days: extract the .25 days
+	mod := int64(eta.Seconds()) % (24 * 3600)
+
+	var daysUntil float64
+
+	// If, even after adding the remainder, the day is still today, calculating days is simple
+	if time.Now().Add(time.Second*time.Duration(mod)).Day() == time.Now().Day() {
+		daysUntil = eta.Hours() / 24
+	} else {
+		// If the remained, e.g. .25 days, causes us to jump over to tomorrow, add a +1 to the days
+		daysUntil = eta.Hours()/24 + 1
+	}
+
+	if daysUntil < 2.0 {
+		// The case of the date being today has already been caught, therefore it's tomorrow
+		return "tomorrow"
+	}
+
+	// Otherwise, just count the days
+	return fmt.Sprintf("in %s", english.Plural(int(daysUntil), "day", "days"))
 }
