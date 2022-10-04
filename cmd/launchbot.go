e00b885fdd84e798930a2b217d14bef52573ec98
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-10-04 22:45:21 +0300

    Proper permission handling for channels
    Turns out that callbacks in channels are _not_ permissioned, and require explicit handling.
    
    * Replace interaction.isGroup with interaction.isPermissioned
    * Handle channel permissions for posts/callbacks separately
    * Bump to 3.2.5

--- cmd/launchbot.go
+++ cmd/launchbot.go
@@ -23,7 +23,7 @@ import (
 // Injected at build-time
 var GitSHA = "0000000000"
 
-const version = "3.2.4"
+const version = "3.2.5"
 
 // Listens for incoming interrupt signals
 func signalListener(session *config.Session) {
