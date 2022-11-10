959d0fa70b0e94d5584ea7619aa8718acdcff2f6
499602D2 <46869890+499602D2@users.noreply.github.com>
2022-11-10 13:03:58 +0200

    Ensure Chat() and Sender() fields exist
    Weird crashes caused by non-admin senders in channels; this is to weed out those errors.

--- cmd/launchbot.go
+++ cmd/launchbot.go
@@ -23,7 +23,7 @@ import (
 // Injected at build-time
 var GitSHA = "0000000000"
 
-const version = "3.2.8"
+const version = "3.2.9"
 
 // Listens for incoming interrupt signals
 func signalListener(session *config.Session) {
