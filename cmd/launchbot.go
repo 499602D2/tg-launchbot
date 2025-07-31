f1831e2c5b43aaf7f7ac5ce44944b985728b40d1
499602D2 <46869890+499602D2@users.noreply.github.com>
2025-07-31 23:58:54 +0300

    Bump version to 3.3.0

--- cmd/launchbot.go
+++ cmd/launchbot.go
@@ -22,7 +22,7 @@ import (
 // Injected at build-time
 var GitSHA = "0000000000"
 
-const version = "3.2.17"
+const version = "3.3.0"
 
 // Listens for incoming interrupt signals
 func signalListener(session *config.Session) {
@@ -130,7 +130,7 @@ func main() {
 	if !noUpdates {
 		// Create a new task scheduler, assign to session
 		session.Scheduler = gocron.NewScheduler(time.UTC)
-		
+
 		// Start the scheduler async
 		session.Scheduler.StartAsync()
 
